9a046c668dbfd638c719e7bd3767b92db8a5f240
Douglas <28629647+Nautman@users.noreply.github.com>
2023-04-23 18:25:14 +0200

    Added saving to back-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,45 @@ app.use(express.json())
 
 const PORT = 3001
 
+// Map for storing lists of todos, key is the id of the list
+const lists = new Map()
+
+// Create initial lists
+lists.set('0000000001', {
+  title: 'My first list',
+  todos: [],
+})
+lists.set('0000000002', {
+  title: 'Second list',
+  todos: [],
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/todos', (req, res) => {
+  const listsWithInfo = new Map()
+  lists.forEach((list, id) => {
+    listsWithInfo.set(id, {
+      ...list,
+      id,
+    })
+  })
+
+  res.send(Object.fromEntries(listsWithInfo.entries()))
+})
+
+app.put('/list/:id', (req, res) => {
+  const { id } = req.params
+  if (!lists.has(id)) {
+    res.status(400).send('List does not exist')
+    return
+  }
+  const { todos } = req.body
+  lists.set(id, {
+    ...lists.get(id),
+    todos,
+  })
+  res.send({ ...lists.get(id), id })
+})
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
