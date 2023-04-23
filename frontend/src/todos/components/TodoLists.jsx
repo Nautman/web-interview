b73286fd0021bb2bf4697c35c5579a8dadf6635c
Douglas <28629647+Nautman@users.noreply.github.com>
2023-04-23 18:27:07 +0200

    Connect front- and back-end saving

--- frontend/src/todos/components/TodoLists.jsx
+++ frontend/src/todos/components/TodoLists.jsx
@@ -10,34 +10,11 @@ import {
 } from '@mui/material'
 import ReceiptIcon from '@mui/icons-material/Receipt'
 import { TodoListForm } from './TodoListForm'
-
-// Simulate network
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
-const fetchTodoLists = () => {
-  return sleep(1000).then(() =>
-    Promise.resolve({
-      '0000000001': {
-        id: '0000000001',
-        title: 'First List',
-        todos: ['First todo of first list!'],
-      },
-      '0000000002': {
-        id: '0000000002',
-        title: 'Second List',
-        todos: ['First todo of second list!'],
-      },
-    })
-  )
-}
+import { useTodoLists } from '../../hooks'
 
 export const TodoLists = ({ style }) => {
-  const [todoLists, setTodoLists] = useState({})
+  const { todoLists, updateList } = useTodoLists()
   const [activeList, setActiveList] = useState()
-
-  useEffect(() => {
-    fetchTodoLists().then(setTodoLists)
-  }, [])
 
   if (!Object.keys(todoLists).length) return null
   return (
@@ -61,11 +38,10 @@ export const TodoLists = ({ style }) => {
         <TodoListForm
           key={activeList} // use key to make React recreate component to reset internal state
           todoList={todoLists[activeList]}
-          saveTodoList={(id, { todos }) => {
-            const listToUpdate = todoLists[id]
-            setTodoLists({
-              ...todoLists,
-              [id]: { ...listToUpdate, todos },
+          saveTodoList={(listId, { todos }) => {
+            updateList({
+              listId,
+              todos,
             })
           }}
         />
