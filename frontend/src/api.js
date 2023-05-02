c47ff1ad8b6c81e08668a1fc41d428badaf67064
Nautman <28629647+Nautman@users.noreply.github.com>
2023-05-02 13:22:30 +0200

    Added two explanatory comments for API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,11 @@
 const baseURL = 'http://localhost:3001'
 
+// Get all todos from all lists
 export function getTodoLists() {
   return fetch(`${baseURL}/todos`).then((response) => response.json())
 }
 
+// Modify a list by it's ID
 export function updateList({ listId, todos }) {
   return fetch(`${baseURL}/list/${listId}`, {
     method: 'PUT',
